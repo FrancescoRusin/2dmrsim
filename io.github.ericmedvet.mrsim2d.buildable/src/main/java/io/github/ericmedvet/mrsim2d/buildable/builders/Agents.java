f893680c0304f20c3d12e3fa9a211b8ede955305
Eric Medvet <eric.medvet@gmail.com>
2025-12-18 15:43:06 +0100

    fix: update code for new output of NDS builders in jdynsym

--- io.github.ericmedvet.mrsim2d.buildable/src/main/java/io/github/ericmedvet/mrsim2d/buildable/builders/Agents.java
+++ io.github.ericmedvet.mrsim2d.buildable/src/main/java/io/github/ericmedvet/mrsim2d/buildable/builders/Agents.java
@@ -23,8 +23,8 @@ package io.github.ericmedvet.mrsim2d.buildable.builders;
 import io.github.ericmedvet.jnb.core.Discoverable;
 import io.github.ericmedvet.jnb.core.Param;
 import io.github.ericmedvet.jnb.datastructure.Grid;
-import io.github.ericmedvet.jsdynsym.buildable.builders.NumericalDynamicalSystems;
 import io.github.ericmedvet.jsdynsym.core.numerical.MultivariateRealFunction;
+import io.github.ericmedvet.jsdynsym.core.numerical.NumericalDynamicalSystem;
 import io.github.ericmedvet.mrsim2d.core.EmbodiedAgent;
 import io.github.ericmedvet.mrsim2d.core.Sensor;
 import io.github.ericmedvet.mrsim2d.core.agents.gridvsr.CentralizedNumGridVSR;
@@ -39,6 +39,7 @@ import io.github.ericmedvet.mrsim2d.core.agents.legged.NumLeggedHybridModularRob
 import io.github.ericmedvet.mrsim2d.core.agents.legged.NumLeggedHybridRobot;
 import io.github.ericmedvet.mrsim2d.core.bodies.Voxel;
 import java.util.List;
+import java.util.function.Function;
 
 @Discoverable(prefixTemplate = "sim|s.agent|a")
 public class Agents {
@@ -49,13 +50,13 @@ public class Agents {
   @SuppressWarnings("unused")
   public static CentralizedNumGridVSR centralizedNumGridVSR(
       @Param("body") GridBody body,
-      @Param("function") NumericalDynamicalSystems.Builder<?, ?> numericalDynamicalSystemBuilder
+      @Param("nds") Function<NumericalDynamicalSystem<?>, NumericalDynamicalSystem<?>> nds
   ) {
     return new CentralizedNumGridVSR(
         body,
-        numericalDynamicalSystemBuilder.apply(
-            MultivariateRealFunction.varNames("x", CentralizedNumGridVSR.nOfInputs(body)),
-            MultivariateRealFunction.varNames("y", CentralizedNumGridVSR.nOfOutputs(body))
+        MultivariateRealFunction.from(
+            CentralizedNumGridVSR.nOfInputs(body),
+            CentralizedNumGridVSR.nOfOutputs(body)
         )
     );
   }
@@ -63,7 +64,7 @@ public class Agents {
   @SuppressWarnings("unused")
   public static DistributedNumGridVSR distributedNumGridVSR(
       @Param("body") GridBody body,
-      @Param("function") NumericalDynamicalSystems.Builder<?, ?> numericalDynamicalSystemBuilder,
+      @Param("nds") Function<NumericalDynamicalSystem<?>, NumericalDynamicalSystem<?>> nds,
       @Param(value = "nOfSignals", dI = 1) int nOfSignals,
       @Param("directional") boolean directional
   ) {
@@ -76,86 +77,14 @@ public class Agents {
                 .get(k)
                 .element()
                 .type()
-                .equals(GridBody.VoxelType.NONE) ? null : numericalDynamicalSystemBuilder.apply(
-                    MultivariateRealFunction.varNames(
-                        "x",
-                        DistributedNumGridVSR.nOfInputs(body, k, nOfSignals, directional)
-                    ),
-                    MultivariateRealFunction.varNames(
-                        "y",
-                        DistributedNumGridVSR.nOfOutputs(body, k, nOfSignals, directional)
-                    )
+                .equals(GridBody.VoxelType.NONE) ? null : MultivariateRealFunction.from(
+                    DistributedNumGridVSR.nOfInputs(body, k, nOfSignals, directional),
+                    DistributedNumGridVSR.nOfOutputs(body, k, nOfSignals, directional)
                 )
         ),
         nOfSignals,
         directional
     );
-  }
-
-  @SuppressWarnings("unused")
-  public static NumIndependentVoxel numIndependentVoxel(
-      @Param("sensors") List<Sensor<? super Voxel>> sensors,
-      @Param(value = "areaActuation", dS = "sides") NumIndependentVoxel.AreaActuation areaActuation,
-      @Param(value = "attachActuation", dB = true) boolean attachActuation,
-      @Param(value = "nOfNFCChannels", dI = 1) int nOfNFCChannels,
-      @Param("function") NumericalDynamicalSystems.Builder<?, ?> numericalDynamicalSystemBuilder
-  ) {
-    return new NumIndependentVoxel(
-        sensors,
-        areaActuation,
-        attachActuation,
-        nOfNFCChannels,
-        numericalDynamicalSystemBuilder.apply(
-            MultivariateRealFunction.varNames("x", NumIndependentVoxel.nOfInputs(sensors, nOfNFCChannels)),
-            MultivariateRealFunction.varNames(
-                "y",
-                NumIndependentVoxel.nOfOutputs(areaActuation, attachActuation, nOfNFCChannels)
-            )
-        )
-    );
-  }
-
-  @SuppressWarnings("unused")
-  public static NumLeggedHybridModularRobot numLeggedHybridModularRobot(
-      @Param("modules") List<AbstractLeggedHybridModularRobot.Module> modules,
-      @Param("function") NumericalDynamicalSystems.Builder<?, ?> numericalDynamicalSystemBuilder
-  ) {
-    return new NumLeggedHybridModularRobot(
-        modules,
-        numericalDynamicalSystemBuilder.apply(
-            MultivariateRealFunction.varNames("x", NumLeggedHybridModularRobot.nOfInputs(modules)),
-            MultivariateRealFunction.varNames("y", NumLeggedHybridModularRobot.nOfOutputs(modules))
-        )
-    );
-  }
-
-  @SuppressWarnings("unused")
-  public static NumLeggedHybridRobot numLeggedHybridRobot(
-      @Param("legs") List<AbstractLeggedHybridRobot.Leg> legs,
-      @Param(value = "trunkLength", dD = 4 * LeggedMisc.TRUNK_LENGTH) double trunkLength,
-      @Param(value = "trunkWidth", dD = LeggedMisc.TRUNK_WIDTH) double trunkWidth,
-      @Param(value = "trunkMass", dD = 4 * LeggedMisc.TRUNK_MASS) double trunkMass,
-      @Param(value = "headMass", dD = LeggedMisc.TRUNK_WIDTH * LeggedMisc.TRUNK_WIDTH * LeggedMisc.RIGID_DENSITY) double headMass,
-      @Param("headSensors") List<Sensor<?>> headSensors,
-      @Param("function") NumericalDynamicalSystems.Builder<?, ?> numericalDynamicalSystemBuilder
-  ) {
-    return new NumLeggedHybridRobot(
-        legs,
-        trunkLength,
-        trunkWidth,
-        trunkMass,
-        headMass,
-        headSensors,
-        numericalDynamicalSystemBuilder.apply(
-            MultivariateRealFunction.varNames("x", NumLeggedHybridRobot.nOfInputs(legs, headSensors)),
-            MultivariateRealFunction.varNames("y", NumLeggedHybridRobot.nOfOutputs(legs))
-        )
-    );
-  }
-
-  @SuppressWarnings("unused")
-  public static ReactiveGridVSR reactiveGridVSR(@Param("body") Grid<ReactiveVoxel> body) {
-    return new ReactiveGridVSR(body);
   }
 
   @SuppressWarnings("unused")
@@ -179,5 +108,74 @@ public class Agents {
             CentralizedNumGridVSR.nOfOutputs(body)
         )
     );
+  }
+
+  @SuppressWarnings("unused")
+  public static NumIndependentVoxel numIndependentVoxel(
+      @Param("sensors") List<Sensor<? super Voxel>> sensors,
+      @Param(value = "areaActuation", dS = "sides") NumIndependentVoxel.AreaActuation areaActuation,
+      @Param(value = "attachActuation", dB = true) boolean attachActuation,
+      @Param(value = "nOfNFCChannels", dI = 1) int nOfNFCChannels,
+      @Param("nds") Function<NumericalDynamicalSystem<?>, NumericalDynamicalSystem<?>> nds
+  ) {
+    return new NumIndependentVoxel(
+        sensors,
+        areaActuation,
+        attachActuation,
+        nOfNFCChannels,
+        nds.apply(
+            MultivariateRealFunction.from(
+                NumIndependentVoxel.nOfInputs(sensors, nOfNFCChannels),
+                NumIndependentVoxel.nOfOutputs(areaActuation, attachActuation, nOfNFCChannels)
+            )
+        )
+    );
+  }
+
+  @SuppressWarnings("unused")
+  public static NumLeggedHybridModularRobot numLeggedHybridModularRobot(
+      @Param("modules") List<AbstractLeggedHybridModularRobot.Module> modules,
+      @Param("nds") Function<NumericalDynamicalSystem<?>, NumericalDynamicalSystem<?>> nds
+  ) {
+    return new NumLeggedHybridModularRobot(
+        modules,
+        nds.apply(
+            MultivariateRealFunction.from(
+                NumLeggedHybridModularRobot.nOfInputs(modules),
+                NumLeggedHybridModularRobot.nOfOutputs(modules)
+            )
+        )
+    );
+  }
+
+  @SuppressWarnings("unused")
+  public static NumLeggedHybridRobot numLeggedHybridRobot(
+      @Param("legs") List<AbstractLeggedHybridRobot.Leg> legs,
+      @Param(value = "trunkLength", dD = 4 * LeggedMisc.TRUNK_LENGTH) double trunkLength,
+      @Param(value = "trunkWidth", dD = LeggedMisc.TRUNK_WIDTH) double trunkWidth,
+      @Param(value = "trunkMass", dD = 4 * LeggedMisc.TRUNK_MASS) double trunkMass,
+      @Param(value = "headMass", dD = LeggedMisc.TRUNK_WIDTH * LeggedMisc.TRUNK_WIDTH * LeggedMisc.RIGID_DENSITY) double headMass,
+      @Param("headSensors") List<Sensor<?>> headSensors,
+      @Param("nds") Function<NumericalDynamicalSystem<?>, NumericalDynamicalSystem<?>> nds
+  ) {
+    return new NumLeggedHybridRobot(
+        legs,
+        trunkLength,
+        trunkWidth,
+        trunkMass,
+        headMass,
+        headSensors,
+        nds.apply(
+            MultivariateRealFunction.from(
+                NumLeggedHybridRobot.nOfInputs(legs, headSensors),
+                NumLeggedHybridRobot.nOfOutputs(legs)
+            )
+        )
+    );
+  }
+
+  @SuppressWarnings("unused")
+  public static ReactiveGridVSR reactiveGridVSR(@Param("body") Grid<ReactiveVoxel> body) {
+    return new ReactiveGridVSR(body);
   }
 }
